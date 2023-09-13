5f3016c8e8f6ad48dd903937d8d5beb081b20255
yucelikiz <97850432+yucelikiz@users.noreply.github.com>
2023-09-13 22:58:20 +0300

    artık yıl dosyası eklendi.

--- artikYil/src/leapYear.java
+++ artikYil/src/leapYear.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class leapYear {
+import java.util.Scanner;
+public class leapYear {
+    public static void main(String[] args){
+
+        //Değişken
+        int year;
+        //Scanner objesi
+        Scanner inpt = new Scanner(System.in);
+
+        //Verileri al
+        System.out.print("Yılı giriniz : ");
+        year = inpt.nextInt();
+
+        //Artık yılı hesapla
+        if (year%4==0 || year%100==0 || year%400==0){
+            System.out.print(year+" bir artık yıldır!");
+        }else {
+            System.out.print(year+" bir artık yıl değildir!");
+        }
+
+    }
 }
